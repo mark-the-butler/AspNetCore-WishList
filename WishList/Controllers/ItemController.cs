84936763e7e3daa708660022181456e27117d046
Mark Butler <mark.d.butler@sherwin.com>
2018-11-13 11:14:29 -0500

    Created item controller

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return Redirect("/Index");
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var itemToDelete = _context.Items.Single(i => i.Id == id);
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+
+            return Redirect("/Index");
+        }
+    }
+}
